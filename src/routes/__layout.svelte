f7f36aafd9081544750033bf0b79a2ae1aaebd3a
Assaf Chamoy <assafchamoy97@gmail.com>
2022-07-14 15:02:00 +0300

    chore: project scaffold

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {fly} from 'svelte/transition';
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<div in:fly class="appContainer">
+    <slot />
+    <footer>Movie App inc</footer>
+</div>
+
+<style>
+    @import '../../static/global.css';
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+    let num = 0;
+
+    const incNumber = () => {
+        if(num < 10) num += 1;
+    } 
+ 
+    const decNumber = () => {
+        if(num > 0) num -= 1;
+    } 
+</script>
+<div>
+    <h1>Count is: {num}</h1>
+    <button on:click={incNumber}>Increment</button>
+    <button on:click={decNumber}>Decrement</button>
+</div>
